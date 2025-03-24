5188dbf6e1a98b2fe89a7ed7ff7dc5391f7cde89
Convolva <95045175+Convolva@users.noreply.github.com>
2025-03-25 03:28:08 +0530

    Refactor/Remove Unnecessary Code

--- app/src/main/java/com/vidya/toodl/features/notes/data/AppDatabase.kt
+++ app/src/main/java/com/vidya/toodl/features/notes/data/AppDatabase.kt
@@ -1,11 +1,7 @@
 package com.vidya.toodl.features.notes.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(
     version = 2,
@@ -13,33 +9,4 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun todoDao(): TaskDao
-
-    companion object {
-
-        val migration_1_2 = object : Migration(1, 2) {
-            override fun migrate(db: SupportSQLiteDatabase) {
-                db.execSQL("ALTER TABLE todo_list_entity ADD COLUMN description varchar(500)")
-            }
-
-        }
-
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        // This method creates (or returns an existing) database instance.
-        fun getDatabase(context: Context): AppDatabase =
-            INSTANCE ?: synchronized(this) {
-                val instance =
-                    Room
-                        .databaseBuilder(
-                            context.applicationContext,
-                            AppDatabase::class.java,
-                            "todo_list_entity",
-                        )
-                        .addMigrations(migration_1_2)
-                        .build()
-                INSTANCE = instance
-                instance
-            }
-    }
 }
